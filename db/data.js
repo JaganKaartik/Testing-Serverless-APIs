290b8bcca2e68754b7f35e3165acc650942f4212
Jagan Kaartik <kaartikjagan@gmail.com>
2022-10-01 21:31:08 +0530

    Updated Data

---
+++ db/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    flight_number: 1,
+    mission_name: "FalconSat",
+    status: "failed",
+    launch_year: "2006",
+    rocket: { rocket_id: "falcon1", orbit: "LEO" },
+    launch_site: { site_name: "Kwajalein Atoll" },
+    cost: 1100,
+  },
+  {
+    flight_number: 14,
+    mission_name: "CRS-3",
+    status: "success",
+    launch_year: "2014",
+    rocket: { rocket_id: "falcon9", orbit: "ISS" },
+    launch_site: { site_name: "CCAFS SLC 40" },
+    cost: 900,
+  },
+  {
+    flight_number: 20,
+    mission_name: "DISCOVR",
+    status: "success",
+    launch_year: "2015",
+    rocket: { rocket_id: "falcon9", orbit: "ES-L1" },
+    launch_site: { site_name: "CCAFS SLC 40" },
+    cost: 1800,
+  },
+  {
+    flight_number: 3,
+    mission_name: "Trailblazer",
+    status: "failed",
+    launch_year: "2008",
+    rocket: { rocket_id: "falcon1", orbit: "LEO" },
+    launch_site: { site_name: "Kwajalein Atoll" },
+    cost: 1000,
+  },
+];
